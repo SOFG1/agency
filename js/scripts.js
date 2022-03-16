1b9452f7dc2b7d6f78f599150c4a70f3d8ed6948
Eddy <eduard.kosian@itsolutions.ee>
2022-03-16 15:24:13 +0400

    Change carette color

--- js/scripts.js
+++ js/scripts.js
@@ -58,7 +58,7 @@ window.onload = function() {
     // INJECT CSS
     var css = document.createElement("style");
     css.type = "text/css";
-    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #FFFF00}";
     document.body.appendChild(css);
 };
 
