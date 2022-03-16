799a00ffb8ebdfcb77b9c2305d13debd0018c867
Eddy <eduard.kosian@itsolutions.ee>
2022-03-16 14:29:52 +0400

    Comlete header

--- js/scripts.js
+++ js/scripts.js
@@ -1,98 +1,105 @@
+// Header Background on scroll
+
+document.addEventListener('scroll', () => {
+    if (window.pageYOffset > 40) document.querySelector('.js-header').classList.add('active');
+    if (window.pageYOffset < 40) document.querySelector('.js-header').classList.remove('active');
+})
+
 
 // Smooth Scroll
 const smoothLinks = document.querySelectorAll('a[data-smooth="smooth"]');
 for (let smoothLink of smoothLinks) {
-	smoothLink.addEventListener('click', function (e) {
-		e.preventDefault();
-		console.log(smoothLink);
-		const id = smoothLink.getAttribute('href');
-		mainNavigation.classList.remove('active');
-		showScroll();
-		document.querySelector(id).scrollIntoView({
-			behavior: 'smooth',
-			block: 'start'
-		});
-	 });
+    smoothLink.addEventListener('click', function(e) {
+        e.preventDefault();
+        console.log(smoothLink);
+        const id = smoothLink.getAttribute('href');
+        mainNavigation.classList.remove('active');
+        showScroll();
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
 };
 
 // Burger
 const mainNavigation = document.getElementById('main-navigation');
 
 document.querySelector('.burger').addEventListener('click', () => {
-	mainNavigation.classList.add('active');
-	hideScroll();
+    mainNavigation.classList.add('active');
+    hideScroll();
 });
 
 document.querySelector('.burger-menu__close').addEventListener('click', () => {
-	mainNavigation.classList.remove('active');
-	showScroll();
+    mainNavigation.classList.remove('active');
+    showScroll();
 });
 
 const hideScroll = () => {
     const scrollWidth = `${getScrollbarWidth()}px`;
 
-	document.body.style.paddingRight = scrollWidth;
-	document.body.style.overflow = 'hidden';
+    document.body.style.paddingRight = scrollWidth;
+    document.body.style.overflow = 'hidden';
 
-	mainNavigation.style.paddingRight = scrollWidth;
+    mainNavigation.style.paddingRight = scrollWidth;
 };
 
 const showScroll = () => {
-	document.body.style.paddingRight = '';
-	document.body.style.overflow = 'visible';
+    document.body.style.paddingRight = '';
+    document.body.style.overflow = 'visible';
 
-	mainNavigation.style.paddingRight = '';
+    mainNavigation.style.paddingRight = '';
 };
 
 const resetNav = () => {
-	mainNavigation.classList.remove('active');
-	showScroll();
+    mainNavigation.classList.remove('active');
+    showScroll();
 }
 
 window.addEventListener('resize', resetNav);
 
 // Get scrollbar width
 const getScrollbarWidth = () => {
-	const outer = document.createElement('div');
+    const outer = document.createElement('div');
 
-	outer.style.position = 'absolute';
-	outer.style.top = '-9999px';
-	outer.style.width = '50px';
-	outer.style.height = '50px'; 
-	outer.style.overflow = 'scroll';
-	outer.style.visibility = 'hidden';
+    outer.style.position = 'absolute';
+    outer.style.top = '-9999px';
+    outer.style.width = '50px';
+    outer.style.height = '50px';
+    outer.style.overflow = 'scroll';
+    outer.style.visibility = 'hidden';
 
-	document.body.appendChild(outer);
-	const scrollBarWidth = outer.offsetWidth - outer.clientWidth;
-	document.body.removeChild(outer);
+    document.body.appendChild(outer);
+    const scrollBarWidth = outer.offsetWidth - outer.clientWidth;
+    document.body.removeChild(outer);
 
-	return scrollBarWidth;
+    return scrollBarWidth;
 }
 
 var swiper = new Swiper(".projectSwiper", {
-	loop: true,
-	loopedSlides: 8,
-	slidesPerView: "auto",
-	spaceBetween: 20,
+    loop: true,
+    loopedSlides: 8,
+    slidesPerView: "auto",
+    spaceBetween: 20,
 
-	autoplay: {
-		delay: 4000,
-	},
+    autoplay: {
+        delay: 4000,
+    },
 });
 
 
 var swiper = new Swiper(".blogSwiper", {
-	loop: true,
-	loopedSlides: 8,
-	slidesPerView: "auto",
-	
-	breakpoints: {
-		575: {
-			spaceBetween: 0,
-		},
+    loop: true,
+    loopedSlides: 8,
+    slidesPerView: "auto",
 
-		320: {
-			spaceBetween: 20,
-		},
-	},
+    breakpoints: {
+        575: {
+            spaceBetween: 0,
+        },
+
+        320: {
+            spaceBetween: 20,
+        },
+    },
 });
